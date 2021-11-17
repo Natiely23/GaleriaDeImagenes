be3140f43e26a5d6610d8a70cd040a1b998ec337
Natiely23 <natiely2303@gmail.com>
2021-11-17 19:48:54 -0400

    Galeria de imagenes

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// VARIABLES 
+let modal = document.querySelector('.modal');
+let images = document.querySelectorAll('.gallery img');
+let modalImg =  document.querySelector('#mymodalimg');
+let imgBox = document.querySelectorAll('.list img'); 
+let caption = document.querySelector('.caption-text');
+let close = document.querySelector('.close');
+
+images.forEach((images) => {
+    images.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = images.src;
+        caption.innerHTML = images.alt;
+
+    });
+});
+
+imgBox.forEach( img => img.addEventListener("click", imgLightbox)); 
+
+function imgLightbox (event){
+    modalImg.src = event.target.src;
+}
+
+close.addEventListener("click", () => {
+    modal.style.display = "none";
+})
